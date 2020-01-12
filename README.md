b188eb156af93560caa11d40f6e2ff3b7adb1f53
Nate Grobe <natepgrobe@gmail.com>
2020-01-11 22:47:32 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#Nate Grobe's Website
+This is a simple website to tell you a little bit about myself and show off a few of my projects
---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// open resume pdf
+document.getElementById("res-button").addEventListener("click", openPDF);
+
+function openPDF(){
+    // swap out the pdf for my resume
+    window.location.href = 'nate-grobe-resume.pdf';
+}
