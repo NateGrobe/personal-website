8ebba17b9d561bd064bf3afbc1bd7d2774e1eccd
Nate Grobe <natepgrobe@gmail.com>
2020-01-15 22:46:00 -0500

    Improved visuals

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,54 @@
-// open resume pdf
-document.getElementById("res-button").addEventListener("click", openPDF);
+// open resume
+$("#res-button").click(function(){
+    window.location.replace('pdf/nate-grobe-resume.pdf');
+})
 
-function openPDF(){
-    // swap out the pdf for my resume
-    window.location.href = 'pdf/nate-grobe-resume.pdf';
-}
+// fade in skills
+$(window).scroll(function() {
+    var hT = $('#skills').offset().top,
+        hH = $('#skills').outerHeight(),
+        wH = $(window).height(),
+        wS = $(this).scrollTop();
+    if (wS-700 > (hT+hH-wH)){
+        $("#skills").fadeIn(1300);
+   }
+});
+
+// fade in projects
+$(window).scroll(function() {
+    var hT = $('#projects').offset().top,
+        hH = $('#projects').outerHeight(),
+        wH = $(window).height(),
+        wS = $(this).scrollTop();
+    if (wS-700 > (hT+hH-wH)){
+        $("#projects").fadeIn(1300);
+   }
+});
+
+// fade in contact bar
+$(window).scroll(function() {
+    var hT = $('#contact').offset().top,
+        hH = $('#contact').outerHeight(),
+        wH = $(window).height(),
+        wS = $(this).scrollTop();
+    if (wS-1400 > (hT+hH-wH)){
+        $("#contact").fadeIn(2000);
+   }
+});
+
+// scroll to skills
+$("#skills-link").click(function(event) {
+    event.preventDefault();
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#skills-pholder").offset().top
+    }, 1000);
+});
+
+// scroll to projects and show skills
+$("#projects-link").click(function(event) {
+    event.preventDefault();
+    $("#skills").fadeIn(1300);
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#projects-pholder").offset().top
+    }, 1000);
+});
